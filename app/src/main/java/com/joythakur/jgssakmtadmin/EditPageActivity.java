90a139fee754364281c8f13c461a2ec71c530abd
stubh505 <kaustubh.sarkar505@gmail.com>
2020-04-30 20:39:41 +0530

    Pre commit to adding card for edit and add page

--- app/src/main/java/com/joythakur/jgssakmtadmin/EditPageActivity.java
+++ app/src/main/java/com/joythakur/jgssakmtadmin/EditPageActivity.java
@@ -1,16 +1,22 @@
 package com.joythakur.jgssakmtadmin;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.joythakur.jgssakmtadmin.ui.model.Page;
+import com.joythakur.jgssakmtadmin.ui.model.Paragraph;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -20,9 +26,22 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditPageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
+    private Integer pageId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +53,24 @@ public class EditPageActivity extends AppCompatActivity implements NavigationVie
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AlertDialog alert = new AlertDialog.Builder(EditPageActivity.this)
+                        .setTitle(R.string.delete_page)
+                        .setMessage(R.string.del_page_body)
+                        .setIcon(R.drawable.ic_delete)
+                        .setPositiveButton(R.string.button_affirmative, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                CallDeleteAPI cd = new CallDeleteAPI();
+                                cd.execute("http://jgssakmtback.herokuapp.com/jgssakmt_backend/PagesAPI/deletePage/"+pageId);
+                            }
+                        })
+                        .setNegativeButton(R.string.button_negative, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).create();
+                alert.show();
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -46,6 +81,11 @@ public class EditPageActivity extends AppCompatActivity implements NavigationVie
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        pageId = getIntent().getIntExtra("pageId", 1001);
+
+        GetPage getPage = new GetPage();
+        getPage.execute("http://jgssakmtback.herokuapp.com/jgssakmt_backend/PagesAPI/getPage/"+pageId);
     }
 
     @Override
@@ -86,4 +126,171 @@ public class EditPageActivity extends AppCompatActivity implements NavigationVie
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private class GetPage extends AsyncTask<String, Void, String> {
+
+        JSONObject jsonObject;
+        Page b;
+
+        @Override
+        protected String doInBackground(String... apis) {
+            try {
+                URL url = new URL(apis[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader ips = new InputStreamReader(inputStream);
+                StringBuilder res = new StringBuilder();
+                int data = ips.read();
+                while (data != -1) {
+                    res.append((char) data);
+                    data = ips.read();
+                }
+
+                jsonObject = new JSONObject(res.toString());
+
+                List<Paragraph> paras = null;
+
+                b = new Page();
+                b.setExcerpt(jsonObject.getString("excerpt"));
+                b.setName(jsonObject.getString("name"));
+                b.setHeader(jsonObject.getString("header"));
+
+                JSONArray jsonArray = jsonObject.getJSONArray("paragraphs");
+
+                if (jsonArray.length()>0) {
+                    paras = new ArrayList<>();
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject para = jsonArray.getJSONObject(i);
+
+                        Paragraph p = new Paragraph();
+                        p.setBody(para.getString("body"));
+                        p.setHeader(para.getString("header"));
+                        p.setImgUrl(para.getString("imgUrl"));
+                        p.setParagraphId(para.getInt("paragraphId"));
+
+                        paras.add(p);
+                    }
+                }
+
+                b.setParagraphs(paras);
+
+                return res.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        public void onPostExecute(String s) {
+            super.onPostExecute(s);
+            setData(b);
+        }
+
+        private void setData(Page b) {
+            EditText pageName = findViewById(R.id.editPageNameEditText);
+            pageName.setText(b.getName());
+            EditText pageHeader = findViewById(R.id.editPageHeaderEditText);
+            pageHeader.setText(b.getHeader());
+            EditText pageExcerpt = findViewById(R.id.editPageExcerptEditText);
+            pageExcerpt.setText(b.getExcerpt());
+
+        }
+
+    }
+
+    private class CallAPI extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString = params[0]; // URL to call
+            String data = params[1];
+
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("PUT");
+                urlConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+                urlConnection.setRequestProperty("Accept", "application/json");
+                urlConnection.connect();
+
+                try(OutputStream os = urlConnection.getOutputStream()) {
+                    byte[] input = data.getBytes(StandardCharsets.UTF_8);
+                    os.write(input, 0, input.length);
+                }
+
+                try(BufferedReader br = new BufferedReader(
+                        new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8))) {
+                    StringBuilder response = new StringBuilder();
+                    String responseLine;
+                    while ((responseLine = br.readLine()) != null) {
+                        response.append(responseLine.trim());
+                    }
+                    //Toast.makeText(EditPageActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+                //urlConnection.connect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        public void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Intent i = new Intent(EditPageActivity.this, AddPageActivity.class);
+            i.putExtra("pageId", pageId);
+            startActivity(i);
+        }
+    }
+
+    private class CallDeleteAPI extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString = params[0];
+
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("DELETE");
+                urlConnection.connect();
+
+                try(BufferedReader br = new BufferedReader(
+                        new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8))) {
+                    StringBuilder response = new StringBuilder();
+                    String responseLine;
+                    while ((responseLine = br.readLine()) != null) {
+                        response.append(responseLine.trim());
+                    }
+                    //Toast.makeText(EditPageActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        public void onPostExecute(String s) {
+            super.onPostExecute(s);
+            finish();
+        }
+    }
+
 }
